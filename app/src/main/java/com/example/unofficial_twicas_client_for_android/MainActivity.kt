cbfadeb4b8d70f427e10ca5f27e288d18ce038f9
t179a <azmtzk179@gmail.com>
2021-12-06 17:08:07 +0900

    created a project.

---
+++ app/src/main/java/com/example/unofficial_twicas_client_for_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unofficial_twicas_client_for_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
